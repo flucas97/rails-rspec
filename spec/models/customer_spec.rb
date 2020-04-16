a646820104a5ab2a5760feafa6c5153504fb5aa0
flucas97 <feijolucas1997@gmail.com>
2020-04-16 20:15:35 -0300

    create spec for name and email

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Customer, :customer, type: :model do
+  let(:customer) { create(:customer) }
+
   it { expect{ create(:customer) }.to change{Customer.all.size}.by(1) }
+
+  it 'Name Test' do
+    expect(customer.name).to eq(Customer.last.name)
+  end
+  it 'Email Test' do
+    expect(customer.email).to eq(Customer.last.email)
+  end
 end
