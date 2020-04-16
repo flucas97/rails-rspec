d7e36c00c4c85f470cd1964a1c5542dbc530a15f
flucas97 <feijolucas1997@gmail.com>
2020-04-16 20:24:33 -0300

    factory bot herance

--- spec/factories/customer.rb
+++ spec/factories/customer.rb
@@ -2,5 +2,13 @@ FactoryBot.define do
     factory :customer do 
         name Faker::Name.name
         email Faker::Internet.email
-    end
+        # heranças
+        factory :customer_vip do 
+            vip true
+        end
+
+        factory :customer_days_to_pay do 
+            days_to_pay 10
+        end
+    end    
 end
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -2,13 +2,29 @@ require 'rails_helper'
 
 RSpec.describe Customer, :customer, type: :model do
   let(:customer) { create(:customer) }
+  let(:customer_vip) { create(:customer_vip) }  
+  let(:customer_days_to_pay) { create(:customer_days_to_pay) }
 
-  it { expect{ create(:customer) }.to change{Customer.all.size}.by(1) }
+  it 'Vip true' do
+    expect(customer_vip.vip).to be true
+  end
+
+  it 'Vip false' do
+    customer_vip.update(vip: false)
+    expect(customer_vip.vip).to be false
+  end
+
+  it 'Days to pay' do
+    expect(customer_days_to_pay.days_to_pay).to eq(10)
+  end
 
   it 'Name Test' do
     expect(customer.name).to eq(Customer.last.name)
   end
+
   it 'Email Test' do
     expect(customer.email).to eq(Customer.last.email)
   end
+
+  it { expect{ create(:customer) }.to change{Customer.all.size}.by(1) }
 end
