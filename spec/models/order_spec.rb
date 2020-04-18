a533d03b9b3d86c7509a5444529e80a20e13a707
flucas97 <feijolucas1997@gmail.com>
2020-04-18 14:02:52 -0300

    create object list with factory

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -3,7 +3,11 @@ require 'rails_helper'
 RSpec.describe Order, :order, type: :model do
     it "Add one item" do
       order = create(:order)
-      p order.description
       expect(order.customer).to be_kind_of(Customer)  
+    end
+
+    it "Three itens" do
+      order = create_list(:order, 5)
+      expect(order.count).to eq(5)  
     end
 end
