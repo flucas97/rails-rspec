c86962fd7826781c1ff9cf9027dcc095bb632f1d
flucas97 <feijolucas1997@gmail.com>
2020-04-20 21:49:31 -0300

    webmock

--- spec/httparty/httpary_spec.rb
+++ spec/httparty/httpary_spec.rb
@@ -1,5 +1,8 @@
 describe 'Httparty', :httparty do
     it 'Get' do
+        stub_request(:get, "https://jsonplaceholder.typicode.com/posts").
+                    to_return(status: 200, body:"", headers: { 'content-type': 'application/json'} )
+
         response = HTTParty.get("https://jsonplaceholder.typicode.com/posts")
         content_type = response.headers['content-type'] 
         expect(content_type).to match(/application\/json/)       
