ad8ac0cc16f2af1f81a1352b18d72a3eb80bd7c7
flucas97 <feijolucas1997@gmail.com>
2020-04-20 22:04:54 -0300

    vcr with rspec

--- spec/httparty/cassette_spec.rb
+++ spec/httparty/cassette_spec.rb
@@ -1,9 +1,7 @@
 describe 'Cassette', :cassette do
-    it 'Save in cassette' do
-        VCR.use_cassette("jsonplaceholder/posts") do
-            response = HTTParty.get("https://jsonplaceholder.typicode.com/posts")
-            content_type = response.headers['content-type'] 
-            expect(content_type).to match(/application\/json/)       
-        end
+    it 'save in cassette', vcr: { cassette_name: 'jsonplaceholder/posts'} do
+        response = HTTParty.get("https://jsonplaceholder.typicode.com/posts")
+        content_type = response.headers['content-type'] 
+        expect(content_type).to match(/application\/json/)       
     end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,7 @@ require 'vcr'
 VCR.configure do |config|
   config.cassette_library_dir = "fixtures/vcr_cassettes"
   config.hook_into :webmock
+  config.configure_rspec_metadata! # you can use VCR with rspec
 end
 
 
