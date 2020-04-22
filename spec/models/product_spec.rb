9da60f966172cf10ac13edf11fa2b73039054f27
flucas97 <feijolucas1997@gmail.com>
2020-04-21 22:06:32 -0300

    clear rspec customer controller

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,19 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Product, :product, type: :model do
-  it 'is valid with description, price and category' do 
-    product = create(:product)
-    expect(product).to be_valid 
+  
+  context 'Validations' do
+    it 'is valid with description, price and category' do 
+      product = create(:product)
+      expect(product).to be_valid 
+    end
+    
+    it { is_expected.to validate_presence_of(:description) }
+
+    it { is_expected.to validate_presence_of(:price) }
+
+    it { is_expected.to validate_presence_of(:category) }
   end
 
-  it { is_expected.to validate_presence_of(:description) }
+  context 'Associations' do 
+    it { is_expected.to belong_to(:category) }
+  end
 
-  it { is_expected.to validate_presence_of(:price) }
-
-  it { is_expected.to validate_presence_of(:category) }
-
-  it 'full description' do
-    product = create(:product)
-    expect(product.full_description).to eq("#{product.description} -- #{product.price} -- #{product.category}}")  
+  context 'Instance Methods' do 
+    it 'full description' do
+      product = create(:product)
+      expect(product.full_description).to eq("#{product.description} -- #{product.price} -- #{product.category}}")  
+    end
   end
 end
