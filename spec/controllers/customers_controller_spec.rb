65d9883b2efadda041acccc0614365679a9e513f
flucas97 <feijolucas1997@gmail.com>
2020-04-21 22:10:05 -0300

    create spec action index

--- spec/controllers/customers_controller_spec.rb
+++ spec/controllers/customers_controller_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe CustomersController, :customer_controller, type: :controller do
-  
+  it '#index' do
+    get :index
+    expect(response).to be_success  
+  end
 end
