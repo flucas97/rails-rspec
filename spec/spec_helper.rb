67d6831508226cba8224aadfd99b95d5ba8ccfd1
flucas97 <feijolucas1997@gmail.com>
2020-04-23 19:06:35 -0300

    include devise controller helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -26,6 +26,7 @@ end
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
+  config.include Devise::Test::COntrollerHelpers, :type => :controller
   # Factory Bot Lint
   config.before(:suite) do 
     FactoryBot.lint
