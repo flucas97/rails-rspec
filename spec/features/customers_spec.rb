9dbfcb27ddbeba8f4dca9076e8c4dfccb0b66468
flucas97 <feijolucas1997@gmail.com>
2020-04-24 16:57:55 -0300

    rspec for visit index page

--- spec/features/customers_spec.rb
+++ spec/features/customers_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
-RSpec.feature "Customers", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+RSpec.feature "Customers", :customer_view, type: :feature do
+  it 'Visit index page' do 
+    visit(customers_path)
+    expect(page).to have_current_path(customers_path)
+  end
 end
