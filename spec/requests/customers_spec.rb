e8b36080fa21ca6e63034a563f09e82ae1a755f4
flucas97 <feijolucas1997@gmail.com>
2020-04-25 16:14:08 -0300

    refactor ID test

--- spec/requests/customers_spec.rb
+++ spec/requests/customers_spec.rb
@@ -1,19 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe "Customers", :customer_request, type: :request do
-  describe "GET /customers" do
+  context "GET /customers" do
     it "index - JSON 200" do
       get "/customers.json"
       expect(response.body).to include_json([
-        id: 1
+        id: /\d/,
+        name: (be_kind_of String),
+        email: (be_kind_of String)
       ])
     end
+
     it "show - JSON 200" do
       get "/customers/1.json"
       expect(response.body).to include_json(
-        id: 1,
-        name: "Tory Gibson",
-        email: "maryam@williamson.org"
+        id: /\d/,
+        name: (be_kind_of String),
+        email: (be_kind_of String)
       )
     end
   end
