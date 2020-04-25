e5c049789425bd16e7d9404b76c85b94594e6e87
flucas97 <feijolucas1997@gmail.com>
2020-04-25 17:46:56 -0300

    create patch and update spec request

--- spec/requests/customers_spec.rb
+++ spec/requests/customers_spec.rb
@@ -21,12 +21,49 @@ RSpec.describe "Customers", :customer_request, type: :request do
     end
 
     it 'create - JSON' do
-      headers = { "ACCEPT" => "application/json"}
       member = create(:member)
-      sign_in member
+      login_as(member, scope: :member)
+
+      headers = { "ACCEPT" => "application/json"}
+
       customers_params = attributes_for(:customer)
       post "/customers.json", params: { customer: customers_params}, headers: headers
-      p response.body
+
+      expect(response.body).to include_json(
+        id: /\d/,
+        name: customers_params[:name],
+        email: customers_params.fetch(:email)
+      )
+    end
+
+    it 'update - JSON' do
+      member = create(:member)
+      login_as(member, scope: :member)
+
+      headers = { "ACCEPT" => "application/json"}
+
+      customers = Customer.first
+      customers.name += " - ATUALIZADO"
+
+      patch "/customers/#{customers.id}.json", params: { customer: customers.attributes}, headers: headers
+
+      expect(response.body).to include_json(
+        id: /\d/,
+        name: customers.name,
+        email: customers.email
+      )
+    end
+
+    it 'destroy - JSON' do
+      member = create(:member)
+      login_as(member, scope: :member)
+
+      headers = { "ACCEPT" => "application/json"}
+
+      customers = Customer.first
+
+      expect{ delete "/customers/#{customers.id}.json", headers: headers }.to change(Customer, :count).by(-1)
+      expect(response).to have_http_status(204)
     end
 
   end
